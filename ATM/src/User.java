6a20eb48788ad1a2f1c3f59ec34aa3328f0600f6
jad524 <58495710+jad524@users.noreply.github.com>
2019-12-12 17:44:08 -0500

    Add files via upload

--- ATM/src/User.java
+++ ATM/src/User.java
@@ -1,29 +1,88 @@
-package ATM.src;
+package ATM;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class User {
 	
-	//private String fName;
-	//private String lName;
-	private String accN;
+	private String fName;
+	private String accN;	
+	private Userpin pin;
 	private float balanceS;
 	private float balanceC;
 	private CheckingAccount checkA;
 	private SavingsAccount saveA;
+
 	
 	
+	public User() {
+		this.fName = "Test";
+		this.accN = 18879;
+		this.pin = "User";
+	}
 	
-	public User(String firstName, String lastName, String accountNumber) {
+	public User(String firstName, String accountNumber, String pin) {
 		
-		//this.fName = firstName;
-		//this.lName = lastName;
-		this.accN = accountNumber;
-		
+		try {
+			this.setFirstName(fName);
+			this.setAccountNumber(accN);
+			this.setPin(pin);
+		} catch(Exception e) {
+			this.fName = "Test";
+			this.accN = "18879";
+			this.pin = "0000";
+		}
 	
+	}
+	//first name
+	public String getFirstName() {
+		return this.fName;
+	}
+	public void setFirstName(String first) throws Exception {
+		if(first==null || first=="") {
+			throw new Exception("Invalid first name");
+		}
+		this.fName=first;
+	}
+	//Account number
+	public int getAccountNumber() {
+		return this.accN;
+	}
+	public void setAccountNumber(String account) throws Exception {
+		if(account.length() < 5 || account.length() > 5) {
+			throw new Exception("Account number is 5 digits");
+		}
+		this.accN = account;
+	}
+	//last name
+	public String getPin() {
+		return this.pin;
+	}
+	public void setPin(String pin) throws Exception {
+		if(pin==null || pin=="" || pin.length() < 4 || pin.length() > 4) {
+			throw new Exception("Invalid pin number");
+		}
+		this.pin=pin;
+	}
+	
+
+	
+	public String toString() {
+		String return_string = "";
+		
+		return_string += "First Name: " + this.fName + "\n";
+		return_string += "Account Number: " + this.accN + "\n";
+		return_string += "Pin: " + this.pin + "\n";
+		return return_string;
 	}
 	
 	
-	public void getCheckingAccount(String aNum) {
+	public getCheckingAccount(String aNum) {
 		
 		//Grab the checking account for the user depeding on their aNum
 		CheckingAccount cAcc = new CheckingAccount();
@@ -37,11 +96,72 @@ public class User {
 		sAcc = saveA;
 	}
 	
+//import csv
+	public static ArrayList<User> importFile(String filepath) {
+		ArrayList<User> userInfo = new ArrayList<User>();
+	
+		File file = new File(filepath);
+		
+		try {
+			Scanner a = new Scanner(file);
+	
+			while (a.hasNextLine()) {
+		String first="";
+			
+			String account="";
+			String pin="";
+			String row= a.nextLine();
+			String[] breaks = row.split(",");
+			
+			try {
+			first=breaks[0];
+			account=breaks[1];
+			pin=breaks[2];
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("need first name, account number, and pin");
+				continue;
+			}
+			catch (NumberFormatException e) {
+				System.out.println("account number is not a number");
+				continue;
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+				continue;
+			}
 
+		User person = new User(first,account, pin);
+			userInfo.add(User);
+			
+		}
+			a.close();
+			
+		}
+		catch(FileNotFoundException e){
+			System.out.print("this file is not found");
+		}
+		return userInfo;
+	}
 	
 		
 		
-	
+//export
+	public static void exportFile(String filepath, ArrayList<User> userInfo) {
+		File file = new File(filepath);
+		try {
+			FileWriter writer = new FileWriter(file);
+			for(int b=0; b<userInfo.size();b++) {
+				Person temp= userInfo.get(b);
+				writer.write(temp.fName + "," + temp.accN + "," + temp.pin +"/n");
+				writer.flush();
+			}
+			writer.close();
+		}
+		catch(IOException e) {
+			System.out.print("Can't write on file");
+		}
+	}
 	
 	
 
